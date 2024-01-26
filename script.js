2adfc608d6f51976ddb47ca5141ec02ff2344588
OktawianMac <maciejewskioktawian@gmail.com>
2024-01-26 22:29:09 +0100

    add autoclicker and pts

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const upgradeButton = document.getElementById('upgradeButton');
 const scoreDisplay = document.getElementById('score');
 const messageDisplay = document.getElementById('message')
 const autoclicker = document.getElementById('autoclicker');
+const pts = document.getElementById('pts');
 
 
-let score = 0;
+let score = 200;
 let clickValue = 1; //początkowa wartość kliknięcie
 let clickUpgradeCost = 10; //Cena bazowa ulepszenia kliknięcia
 let clickUpgradeCount = 0; //Ilośc zakupionych ulepszeń kliknięcia
 let autoclickerUpgradeCost = 200; //Cena bazowa autoclickera
 let autoclickerUpgradeCount = 0; //Ilośc zakupionych autoclickerow
 
-
+let autoClickerInterval;
 
 
 
@@ -37,7 +38,7 @@ clickButton.addEventListener('click', () => {
 
 //upgrade podstawowego przyciksu
 upgradeButton.addEventListener('click', () => {
-    if (score >= clickUpgradeCost ) {
+    if (score >= clickUpgradeCost) {
 
         clickValue += 1;
 
@@ -57,22 +58,34 @@ upgradeButton.addEventListener('click', () => {
 });
 
 function addAutoclicker() {
+    if (score >= autoclickerUpgradeCost) {
 
-    autoclickerUpgradeCount += 1;
-    score -= autoclickerUpgradeCost;
-    autoclickerUpgradeCost = Math.ceil(200 * Math.pow(1.15, autoclickerUpgradeCount));
+        autoclickerUpgradeCount += 1;
+        score -= autoclickerUpgradeCost;
+        autoclickerUpgradeCost = Math.ceil(200 * Math.pow(1.15, autoclickerUpgradeCount));
+    } else {
+        showMessage("Nie masz wystarczającej liczby punktów, aby kupić to ulepszenie");
+    }
+};
 
-    setInterval(() => {
-        score += autoclickerUpgradeCount * 1.6;
+
+
+function updateInterval() {
+    clearInterval(autoClickerInterval);
+    autoClickerInterval = setInterval(() => {
+        score += autoclickerUpgradeCount * 0.8;
         updateScore();
-    }, 2000);
-
+        updatePTS();
+    }, 1000);
 }
+
+
 
 autoclicker.addEventListener('click', () => {
     addAutoclicker();
     updateAutoclicker();
     updateScore();
+    updateInterval();
 })
 
 
@@ -87,7 +100,9 @@ autoclicker.addEventListener('click', () => {
 
 
 
-
+function updatePTS() {
+    pts.textContent = `PTS: ${autoclickerUpgradeCount * 0.8}`;
+}
 
 //upadte przycisku do ulepszenia
 function updateUpgradeButton() {
@@ -105,6 +120,8 @@ function updateScore() {
     scoreDisplay.textContent = `Wynik: ${score.toFixed(2)}`;
 
 }
+
+
 
 function showMessage(message) {
 
